cb1d41eef51a9af322c41972248bec8542d2cf83
Dernorayytor <106371448+Dernorayytor@users.noreply.github.com>
2023-02-01 12:47:50 +0700

    Update connect.php

--- BIN/PHP/connect.php
+++ BIN/PHP/connect.php
@@ -14,5 +14,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-else { echo "Connected to mysql database. <br>"; }
+// else { echo "Connected to mysql database. <br>"; }
 ?>
